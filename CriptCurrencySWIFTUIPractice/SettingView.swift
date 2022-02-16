789ca4421eaaade81cc62415fa9df5be19266960
practice20200 <wor9hard2018@gmail.com>
2022-02-15 23:04:08 -0800

    2nd Commit Created SettingView

---
+++ CriptCurrencySWIFTUIPractice/SettingView.swift
@@ -0,0 +1,44 @@
+//
+//  SettingView.swift
+//  CriptCurrencySWIFTUIPractice
+//
+//  Created by Apple New on 2022-02-15.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        
+        List{
+            Section{
+                SettingItemView(title: "All")
+                SettingItemView(title: "Currencies")
+                SettingItemView(title: "Coins")
+                SettingItemView(title: "Gold")
+            }
+            
+            Section{
+                SettingItemView(title: "Change Language")
+            }
+        }
+    }
+}
+
+struct SettingItemView: View{
+    var title: String
+    var body: some View {
+        NavigationLink{
+            Text("title")
+                
+        } label: {
+            Text(title)
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
