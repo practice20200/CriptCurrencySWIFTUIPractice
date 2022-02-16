036f8ccf0c7af5b81cf1684fe2d678b7200172dd
practice20200 <wor9hard2018@gmail.com>
2022-02-15 22:25:43 -0800

    Initial Commit

---
+++ CriptCurrencySWIFTUIPractice/CriptCurrencySWIFTUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CriptCurrencySWIFTUIPracticeApp.swift
+//  CriptCurrencySWIFTUIPractice
+//
+//  Created by Apple New on 2022-02-15.
+//
+
+import SwiftUI
+
+@main
+struct CriptCurrencySWIFTUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
