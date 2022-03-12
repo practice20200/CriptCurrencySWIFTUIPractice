1b09336d072f2db1db00ccb241c160d1e56346db
practice20200 <wor9hard2018@gmail.com>
2022-03-11 16:10:52 -0800

    Created two models and one dataProvider

---
+++ CriptCurrencySWIFTUIPractice/Models/IndexTitle.swift
@@ -0,0 +1,8 @@
+//
+//  IndexTitle.swift
+//  CriptCurrencySWIFTUIPractice
+//
+//  Created by Apple New on 2022-03-11.
+//
+
+import Foundation
---
+++ CriptCurrencySWIFTUIPractice/Resources/CurrencyDataProvider.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyDataProvider.swift
+//  CriptCurrencySWIFTUIPractice
+//
+//  Created by Apple New on 2022-03-11.
+//
+
+import Foundation
